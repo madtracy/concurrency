c368aeddf0104079e6d4bbff6fb89d881d576cea
madtracy <madtracy@gmail.com>
2014-08-14 23:47:48 +0800

    commit test

---
+++ src/main/java/concurrency/temp/ComitTest.java
@@ -0,0 +1,11 @@
+package concurrency.temp;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tracy
+ * Date: 14-8-14
+ * Time: 下午11:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComitTest {
+}
